c97ea73599e19cd16a849cb3bf1054bb29271a3f
= <=>
2018-11-08 08:43:08 +0800

    完成首页的登录

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 const Towxml = require('/towxml/main');
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    onLaunch: function() {
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+    },
+    // 登录,
+    // 这里的登录并不会在app加载过程中调用，而是作为一个全局方法，在各个页面中调用
+    // 参数userInfo是页面中通过登录button传递过来的用户信息
+    login: function(userInfo) {
+        var that = this;
+        wx.showLoading({
+          title: '登录中...',
+        })
+        wx.login({
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+                // 发送 res.code 到后台换取 openId, token
+                wx.request({
+                    url: that.globalData.requesturl + '/login', //仅为示例，并非真实的接口地址
+                    data: {
+                        code: res.code,
+                        userinfo: userInfo
+                    },
+                    header: {
+                        'content-type': 'application/json' // 默认值
+                    },
+                    success(res) {
+                        console.log(res.data);
+                        //登录成功，1.把userinfo保存到全局的globalData中 1.把token保存在本地
+                        wx.hideLoading()
+                        that.globalData.userInfo = userInfo;
+                        try {
+                            wx.setStorageSync('key', 'value')
+                        } catch (e) {}
+                    }
+                })
             }
-          })
-        }
-      }
-    })
-  },
-  towxml:new Towxml(),
-  globalData: {
-    userInfo: null
-  }
+        })
+
+    },
+    towxml: new Towxml(),
+    globalData: {
+        userInfo: null,
+        requesturl: "http://localhost:3009"
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -21,34 +21,14 @@ Page({
                 loginmodal: true
             })
         }
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            })
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
-                })
-            }
-        } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
-            wx.getUserInfo({
-                success: res => {
-                    app.globalData.userInfo = res.userInfo
-                    this.setData({
-                        userInfo: res.userInfo,
-                        hasUserInfo: true
-                    })
-                }
-            })
-        }
-
-
+    },
+    // 点击登录按钮
+    onGotUserInfo: function(e) {
+        this.setData({
+            loginmodal:false
+        })
+        console.log(e.detail.userInfo)
+        app.login(e.detail.userInfo)
     },
     getUserInfo: function(e) {
         console.log(e)
@@ -70,16 +50,16 @@ Page({
         console.log(e)
     },
     //打开登录的模态框
-    showModal: function(e) {
+    showLoginModal: function(e) {
         var showName = e.currentTarget.dataset.modal;
         this.setData({
-            modalName: showName
+            loginmodal: true
         })
     },
     //关闭登录的模态框
-    closeModal: function(e) {
+    closeLoginModal: function(e) {
         this.setData({
-            modalName: null
+            loginmodal: false
         })
     },
 })
